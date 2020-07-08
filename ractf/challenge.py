5c316416837900fb4385be2e5823d7bb044ea357
BenTechy66 <sendbenspam@yahoo.co.uk>
2020-07-08 23:59:30 +0100

    Files

--- ractf/challenge.py
+++ ractf/challenge.py
@@ -1,6 +1,7 @@
 from .abc import APIBaseObject
 from .helpers.requests import post
 from .hint import Hint
+from .file import File
 
 
 class Challenge(APIBaseObject):
@@ -9,6 +10,7 @@ class Challenge(APIBaseObject):
         self.ctf = ctf
         super(Challenge, self).__init__(ctf, data=data)
         self.convert_json_to_hints()
+        self.convert_json_to_files()
 
     def get_api_path(self):
         return f"challenges/{self.id}/"
@@ -20,6 +22,14 @@ class Challenge(APIBaseObject):
                 continue
             hints.append(Hint(hint["id"], self, self.ctf, data=hint))
         self.hints = hints
+    
+    def convert_json_to_files(self):
+        files = []
+        for instance in self.files:
+            if isinstance(instance, File):
+                continue
+            files.append(File(instance["id"], self, self.ctf, data=instance))
+        self.files = files
     
     def submit_flag(self, flag):
         resp = post("challenges/submit_flag/", self.ctf, json={"challenge": self.id, "flag": flag})
